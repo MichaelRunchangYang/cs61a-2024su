ec14abd97dd3f85d2dacd6b5e31f9682c81e04eb
MichaelRunchangYang <runchangyang@gmail.com>
2024-10-09 00:11:58 +0800

    lab09_finished

--- cs61a/lab09/lab09.scm
+++ cs61a/lab09/lab09.scm
@@ -1,27 +1,47 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (cond ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        (else 1))
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+  (lambda (inc) (+ num inc))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+  (lambda (x) (f (g x)))
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (lambda (x) (if (= n 1) (f x) (begin (define x (f x)) ((repeat f (- n 1)) x))))
+)
 
-(define (max a b)
-  (if (> a b)
-      a
-      b))
+(define (max a b) (if (> a b) a b))
+(define (min a b) (if (> a b) b a))
+(define (gcd a b)
+  (cond
+        ((zero? (modulo (max a b) (min a b))) (min a b))
+        (else (gcd (modulo (max a b) (min a b)) (min a b))))
+)
 
-(define (min a b)
-  (if (> a b)
-      b
-      a))
-
-(define (gcd a b) 'YOUR-CODE-HERE)
-
-(define (duplicate lst) 'YOUR-CODE-HERE)
+(define (duplicate lst)
+  (define (helper lst result)
+    (if (null? lst)
+        result
+        (helper (cdr lst) (append result (list (car lst) (car lst))))))
+  (helper lst '()))
 
 (expect (duplicate '(1 2 3)) (1 1 2 2 3 3))
-
 (expect (duplicate '(1 1)) (1 1 1 1))
 
-(define (deep-map fn s) 'YOUR-CODE-HERE)
+(define (deep-map fn s)
+  (cond
+    ((null? s) '())
+    ((list? (car s))
+     (cons (deep-map fn (car s))
+           (deep-map fn (cdr s))))
+    (else
+     (cons (fn (car s))
+           (deep-map fn (cdr s)))))
+)
+
